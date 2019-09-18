2c90822a0b8db1c9966192f3b38e472d59220f1b
Mr_XiD <44462886+Xidploit@users.noreply.github.com>
2019-09-18 20:38:00 +0700

    Add files via upload

---
+++ tools.php
@@ -0,0 +1,154 @@
+
+<html>
+	<head>
+		<title>Codexrush Hacking Tools</title>
+         <meta name='description' content='let's join us!' />
+         <link href="code.css" rel="stylesheet" type="text/css">
+         <link href="https://fonts.googleapis.com/css?family=Metal Mania|Ceviche+One|Nosifer|Iceberg|Rancho|Iceland|Nova+Mono|Libre+Barcode+39+Extended+Text" rel="stylesheet" type="text/css"> 
+  <link href="https://fonts.googleapis.com/css?family=Chivo&display=swap" rel="stylesheet">
+          <body>
+          	
+          	<style>
+   	body {
+       background-color: black;
+       background-size: cover;
+       background-repeat: no-repeat;
+       background-attachment: fixed;
+       color: white;
+       text-align: center;
+       font-family: Chivo;
+      font-size: 20px;
+      }
+    a {
+    color: white;
+    text-decoration: none;
+    font-size: 27px;
+     }
+    a:-webkit-any-link {
+    color: #ffffff;
+    font-family: Chivo;
+    font-size: large;
+    text-decoration: blink;
+     }
+    a:hover {
+    color: #ffffff;
+    font-family: Georgia;
+    text-shadow: 0 0 15px white;
+     }
+          .header {
+    background: black;
+    border: 2px solid red;
+    border-radius: 10px;
+    padding: 10px;
+    }
+           .menu {
+    background: black;
+    font-family: Georgia;
+    font-size:25px;
+    text-align: center;
+    border: 2px solid red;
+    border-radius: 20px;
+    color: #ff0000;
+    padding: 30px;
+    margin: 5px;
+     }
+     .menu_tools {
+    background: #222222;
+    text-align: center;
+    border: 2px solid red;
+    box-shadow: red 1px 1px 20px;
+    border-radius: 7px;
+    color: #ff0000;
+    padding: 40px;
+    margin: 5px;
+     }
+   a:hover
+{
+   background:red;
+   background:-moz-linear-gradient(top, black 0%, maroon 90%);
+   background:-webkit-gradient(linear, left top, left bottom,       color-stop(0%,black), color-stop(90%,maroon));
+   background:-webkit-linear-gradient(top, black 0%,maroon 90%);
+   background:-o-linear-gradient(top, black 0%,maroon 90%);
+   background:-ms-linear-gradient(top, black 0%,maroon 90%);
+   filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='black', endColorstr='maroon',GradientType=0 );
+   background:linear-gradient(top, black 0%,maroon 90%);
+   border:1px solid red;
+   border-radius: 50px/100px;
+   color: red;
+   cursor: pointer;
+   text-shadow: white 1px 1px 4px;
+   box-shadow: white 1px 5px 4px;
+   }
+a{
+	text-align:center;
+    display: inline-block;
+;
+    border: 2px solid red;
+    box-shadow:red 1px 1px 21px;
+    border-radius: 50px/100px;
+    max-width: 100%;
+    margin: 0 auto;
+    padding: 10px;
+    background: repeat-x center bottom #470001;
+    color: #FFFFFF;
+}
+c{
+    text-align:center;
+    display: inline-block;
+    border: 2px solid red;
+    border-radius: 50px/100px;
+    max-width: 100%;
+    margin: 0 auto;
+    padding: 10px;
+    background: repeat-x center bottom #470001;
+    color: #FFFFFF;
+}
+
+</style>
+</body>
+       	<div class="header"> 
+           <img src="https://i.ibb.co/sKKdLj1/IMG-20190828-222220.png" width="500" weight="500">
+           </div>
+           <div class="header">
+           <marquee><x>Gunakan tools ini dengan baik. dan  mohon maaf jika ada tools yang error , karena tools ini masih dalam pengembangan.  terimakasih :)</x></marquee>
+             <br>
+             </div>	
+             <div class="menu">
+            <c>Tools Online</c>
+           <div class="menu_tools">
+           <a href="hackbar.php">Hackbar SQLi</a>
+        <a href="wpbrute.php">Wp Brute Force</a>
+		<a href="joomla.php">Joomla 3.8 exploit</a>
+		<a href="admin-finder.php">Admin Finder</a>
+		<a href="cooming.php">Csrf Online</a>
+		<a href="lokoxploit.php">Lokomedia Exploit</a>
+		<a href="balitbang.php">Hash Pass Balitbang</a>
+		<a href="csrf-balit.php">Csrf Sql Balitbang</a>
+		<a href="cooming.php">Html Encryption</a>
+		<a href="cooming.php?php=id?9">YouTube Dowloader</a>
+		<a href="hash.php">Md5 & PhpBB Encryption</a>
+		<a href="webdav.php">Webdav Checker</a>
+		<a href="change-pass.php">Change Pass Webmail</a>
+              </div>
+           <br>
+         	<c>Defacer Notifier</c>
+         <div class="menu_tools">
+       <a href="http://zone-h.org">Zone-H</a>
+       <a href="http://mirror-h.org">Mirror-H</a>
+       <a href="http://defacer.id">Defacer-ID</a>
+       <a href="http://hackersid.com">HackersID</a>
+       <a href="http://zone-d.org">Zone-D</a>
+           </div>
+           <br>
+            <c>Shell & Source Code</c>
+            <div class="menu_tools">
+         <a href="cooming.php">Indoxploit shell</a>
+         <a href="cooming.php">ITsecTeamSh3ll</a>
+         <a href="cooming.php">Obyt3m1n1 shell</a>
+         <a href="cooming.php">Mister_XID Sh3ll</a>
+         <a href="cooming.php">C99 Sh3ll</a>     
+           </div>
+           </div>
+           <center><a href="https://twitter.com/wolf46russia?ref_src=twsrc^tfw" class="twitter-follow-button" data-size="large" data-show-screen-name="false" data-show-count="false">Follow @wolf46russia</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script><br></center>
+</font> <font size="2>" font color="#CECECE" face="Georgia">Copyright</font> <font size="2"><script>document.write(new Date().getFullYear())</script> </font><font size="2>" font color="#CECECE" face="Georgia">All rights reserved. codexrush</font></font>
+           
